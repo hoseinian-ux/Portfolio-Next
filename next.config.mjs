4771812f6e954db4cb6a4cf36dd52f086a8db8b4
hoseinian <a.hoseinian13@gmail.com>
2025-08-11 02:53:27 +0330

    Add

--- next.config.mjs
+++ next.config.mjs
@@ -19,5 +19,13 @@ const nextConfig = {
     ],
   },
 }
+const path = require('path');
+
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 export default nextConfig
