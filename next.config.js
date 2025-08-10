7a58f4cadb0937daf7ab6029ea6a446173839c0f
hoseinian <a.hoseinian13@gmail.com>
2025-08-11 03:04:39 +0330

    next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'moviesapi.ir', pathname: '/**' },
+      { protocol: 'https', hostname: 'i.dummyjson.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'fakestoreapi.com', pathname: '/img/**' },
+    ],
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
